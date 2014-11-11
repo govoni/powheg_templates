c2dd279579abbd01d75ba8c8e2f19b0d99deb45f
Pietro Govoni <govoni@lxplus0200.cern.ch>
2014-11-11 09:36:19 +0100

    setup on my lxplus

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export PATH=/afs/cern.ch/user/g/govoni/work/HiggsPlusJets/fastjet/bin/:$PATH 
+export PATH=/afs/cern.ch/user/g/govoni/work/HiggsPlusJets/lhapdf/bin/:$PATH 
+export LD_LIBRARY_PATH=/afs/cern.ch/user/g/govoni/work/HiggsPlusJets/fastjet/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/user/g/govoni/work/HiggsPlusJets/lhapdf/lib/:$LD_LIBRARY_PATH
+export LHAPATH=/afs/cern.ch/user/g/govoni/work/HiggsPlusJets/lhapdf/share/lhapdf/PDFsets
